b5e8fd82ee402877085615a57afc7bce8dfdd7d6
Ana <anafvana@gmail.com>
2022-08-12 23:30:42 +0200

    🐛 FIX: Hack around GUI and ALT being switched

--- keymap.c
+++ keymap.c
@@ -60,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LALT,
-    _______, _______, _______, KC_LCTL, KC_LGUI, KC_SPC,  KC_SPC,  RAISE,   LOWER,   _______, _______, _______
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LGUI,
+    _______, _______, _______, KC_LCTL, KC_LALT, KC_SPC,  KC_SPC,  RAISE,   LOWER,   _______, _______, _______
 ),
 
 /* Lower
